3ffa5a43bd3664e263a028c34337a74e9aa36997
dionis <omsk1984@gmail.com>
2026-05-12 11:07:00 +0300

    fixed

--- tests/run_test.ps1
+++ tests/run_test.ps1
@@ -1,4 +1,4 @@
 cd tests/MarketDataCollector.Tests
-dotnet test MarketDataCollector.Tests.csproj --verbosity detailed 2>&1
-
+dotnet test MarketDataCollector.Tests.csproj --no-build -v n #--verbosity detailed
+#fff
 Read-Host -Prompt "Нажмиете любую клавишу для выхода"
