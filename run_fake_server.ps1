402f2c5b4816e0af896b8678e8cf94bb9c238a38
dionis <omsk1984@gmail.com>
2026-05-19 09:17:51 +0300

    fix

--- run_fake_server.ps1
+++ run_fake_server.ps1
@@ -1,9 +1,9 @@
 # Параметры (можно менять под свои нужды)
 $Port = 5000
-$Rps = 5000
+$Rps = 1500
 $Symbols = "btcusdt,ethusdt,solusdt"
 $BasePrice = 5000
-$MaxTicks = 300000
+$MaxTicks = 20000
 
 # Останавливаем предыдущий экземпляр, если запущен
 $proc = Get-Process -Name "FakeTickServer" -ErrorAction SilentlyContinue
