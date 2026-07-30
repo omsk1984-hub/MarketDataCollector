842797fc6c744370ef6ec5e2f6962b2163aa3bc4
dionis <omsk1984@gmail.com>
2026-07-30 17:35:56 +0300

    fix

--- run_fake_server.ps1
+++ run_fake_server.ps1
@@ -3,7 +3,7 @@ $Port = 5000
 $Rps = 20000
 $Symbols = "btcusdt,ethusdt,solusdt"
 $BasePrice = 5000
-$MaxTicks = 1200000
+$MaxTicks = 1500000
 $DupPercent = 3
 
 # Останавливаем предыдущий экземпляр, если запущен
